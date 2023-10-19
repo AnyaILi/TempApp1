f63a834c2b0e0b50b46433ff886732530e93c5e2
Anya <anyali0219@gmail.com>
2023-10-19 00:10:11 -0700

    Initial Commit

---
+++ TempApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempApp1
+//
+//  Created by Anya Li on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempApp1/TempApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TempApp1App.swift
+//  TempApp1
+//
+//  Created by Anya Li on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TempApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
